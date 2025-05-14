9035c4ed572d08ff912aaed39527f65889272f9f
sojunghoon <sojw8579@naver.com>
2025-05-14 15:00:49 +0900

    3:00

--- gda_jsp_project/src/main/java/sitemesh/SiteMeshFilter.java
+++ gda_jsp_project/src/main/java/sitemesh/SiteMeshFilter.java
@@ -17,9 +17,10 @@ import org.sitemesh.config.ConfigurableSiteMeshFilter;
 public class SiteMeshFilter extends ConfigurableSiteMeshFilter {
 	@Override
 	protected void applyCustomConfiguration(SiteMeshFilterBuilder builder) {
-		builder.addDecoratorPath("/user/*", "/layout/layout.jsp")		
+		builder.addDecoratorPath("/user/*", "/layout/layout.jsp")
+		.addDecoratorPath("/projects/*", "/layout/layout.jsp")	
 		.addExcludedPath("/user/login*")
-    .addExcludedPath("/user/signupform")
+		.addExcludedPath("/user/signupform")
 		.addExcludedPath("/static/css/*")
 		.addExcludedPath("/static/js/*")
 		.addExcludedPath("/static/images/*");
